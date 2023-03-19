df81b0e2f9bfef57490ae516722d6b9e9dc27231
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-19 15:08:42 -0400

    Finished background picker
    Added fourth background.
    Added styles to background picker.
    Changed fixed background options to gradient animations.
    Added hover animation.

--- class12-materials 2/background-picker/js/main.js
+++ class12-materials 2/background-picker/js/main.js
@@ -1,19 +1,37 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+document.getElementById('first').onclick = partyPurple
+document.getElementById('second').onclick = partyGreen
+document.getElementById('third').onclick = partyBlue
+document.getElementById('fourth').onclick = partyYellow
 
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
   document.querySelector('body').style.color = 'white'
+  document.querySelector('body').classList.add("firstBg");
+  document.querySelector('body').classList.remove("secondBg");
+  document.querySelector('body').classList.remove("thirdBg");
+  document.querySelector('body').classList.remove("fourthBg");
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
   document.querySelector('body').style.color = 'white'
+  document.querySelector('body').classList.add("secondBg");
+  document.querySelector('body').classList.remove("firstBg");
+  document.querySelector('body').classList.remove("thirdBg");
+  document.querySelector('body').classList.remove("fourthBg");
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
   document.querySelector('body').style.color = 'white'
+  document.querySelector('body').classList.add("thirdBg");
+  document.querySelector('body').classList.remove("secondBg");
+  document.querySelector('body').classList.remove("firstBg");
+  document.querySelector('body').classList.remove("fourthBg");
+}
+
+function partyYellow() {
+  document.querySelector('body').style.color = 'white'
+  document.querySelector('body').classList.add("fourthBg");
+  document.querySelector('body').classList.remove("secondBg");
+  document.querySelector('body').classList.remove("thirdBg");
+  document.querySelector('body').classList.remove("firstBg");
 }
