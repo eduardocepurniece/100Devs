ea8cd5287d6004b6fad840f7d2004b919b4b166e
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-21 11:33:57 -0400

    Finished temperature converter

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,15 @@
 //Write your pseduo code first! 
+
+document.getElementById('convert').addEventListener('click', convert);
+
+function convert(){
+//read input number
+let input = parseInt(document.getElementById('celcius').value);
+// convert
+input = input * 5/9 + 32;
+//show result
+document.getElementById('result').innerText = input;
+}
+
+
+
