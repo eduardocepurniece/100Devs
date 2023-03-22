4435562da1cf5d2d7793b925c8afb9c189d0d9f8
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-22 08:50:46 -0400

    Completed simple functions practice

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,12 @@
 //create a function that adds two numbers and alerts the sum
-
+function add2(n1, n2){
+    alert(n1 + n2);
+}
 //create a function that multiplys three numbers and console logs the product
-
+function multiply3(n1, n2, n3){
+    console.log(n1 * n2 * n3);
+}
 //create a function that divides two numbers and returns the ???
+function divide(n1, n2){
+    return n1 / n2;
+}
