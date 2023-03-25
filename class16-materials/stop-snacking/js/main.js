fd173ef283b766cd23fcf775e53f34efe98d57aa
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-25 06:55:39 -0400

    Finished stop snacking

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+document.getElementById('help').addEventListener('click', hungry);
+
+function hungry(){
+    const amountOfHelp = parseInt(document.querySelector('input').value);
+    document.querySelector('#stops').innerHTML = '';
+    for(let i = 0; i < amountOfHelp; i++){
+        document.querySelector('#stops').innerHTML += 'Stop! '
+    }
+}
