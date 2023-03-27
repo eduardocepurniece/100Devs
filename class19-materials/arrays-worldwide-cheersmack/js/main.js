f6d7ffaa1f60be2cd83f553be28c63dd7fcfb59a
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-27 15:30:44 -0400

    Completed Worlwide cheersmack exercices

--- class19-materials/arrays-worldwide-cheersmack/js/main.js
+++ class19-materials/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,10 @@
 //Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+function arrCreator(n){
+    let arr = []
+    for(let i = 1; i <= n; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+console.log(arrCreator(7));
