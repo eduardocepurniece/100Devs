d5f3406a999f4bddc2aa23a4d593233803b4a81f
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-28 16:20:58 -0400

    Completed review summer games exercices

--- class20-materials/review-summer-games/js/main.js
+++ class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,6 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiArr(arr){
+    alert(arr.reduce((prod, curr) => prod * curr));
+}
+
+multiArr([2,4,5]);
