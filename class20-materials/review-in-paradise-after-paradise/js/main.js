000da45aa119880f22eb2fae581fd7bfd573bf7c
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-28 16:20:28 -0400

    completed review in paradise after paradise exercices

--- class20-materials/review-in-paradise-after-paradise/js/main.js
+++ class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,8 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function storeGreet(arr){
+    if(arr[1] < arr[arr.length - 1]) alert('Hi');
+    else if(arr[1] > arr[arr.length - 1]) alert('Bye');
+    else alert('We close in an hour');
+}
+
+storeGreet([1,2,3,4,5,7,3,2,8]);
