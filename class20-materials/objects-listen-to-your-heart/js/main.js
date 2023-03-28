64ad655bdfd86ec0c1bbe8d151dfc60c8772e8eb
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-28 16:18:57 -0400

    Completed objects listen to your heart exercices

--- class20-materials/objects-listen-to-your-heart/js/main.js
+++ class20-materials/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,11 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    color: 'black',
+    brand: 'CASlO',
+    model: 'free dive 5800 k14',
+    style: 'business casual',
+
+    tellTime : function(){ console.log('The time is nigh')},
+    resetTime : function(){ console.log('Time is back to 0:00')},
+    alarm : function(){ console.log('BANG! BANG! BANG! BANG!')}
+}
