3a81c7d31faaa4650afd2635d7460d5c827f54e6
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-29 17:03:56 -0400

    Completed Objects zeo exercices

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,28 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    color : 'black',
+    shape : 'round',
+    brand : 'MAKO',
+    rope : true,
+    currentMinutes: 5,
+    currentSeconds: 26,
+    currentMiliSeconds: 13,
+
+    resetTime : function (){
+        this.currentMiliSeconds = 0;
+        this.currentMinutes = 0;
+        this.currentSeconds = 0;
+    },
+
+    start : function (){
+        console.log('time starta');
+    },
+
+    stop : function (){
+        console.log('time stoptaaa');
+    },
+
+    changeMode : function (){
+        console.log('changed mode! HIIIAAAAAJ!')
+    },
+}
