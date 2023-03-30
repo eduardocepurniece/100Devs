571ef7c437820995495624f37aff5e5db1501c46
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-30 15:15:59 -0400

    Completed objects lost galaxy exercices

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {
+    color : 'black',
+    brand : 'dell',
+    wireless : false,
+    dpis : 750,
+
+    leftClick : function(){
+        console.log('left click');
+    },
+    rightClick : function(){
+        console.log('right click');
+    },
+    scroll : function(){
+        console.log('They see me scrolling... They hating...');
+    }
+}
