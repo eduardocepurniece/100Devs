f3afb746e31639d10c263befad1e84d4186ca3b2
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-30 15:16:38 -0400

    Completed objects street fighter

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,17 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function CreateCharacter(name, job, nativeSkill, pet){
+    this.name = name;
+    this.job = job;
+    this.nativeSkill = nativeSkill;
+    this.pet = pet;
+
+    this.attack = function(){
+        console.log('Slash');
+    }
+    this.move = function(){
+        console.log('moving');
+    }
+    this.jump = function(){
+        console.log('jumping');
+    }
+}
