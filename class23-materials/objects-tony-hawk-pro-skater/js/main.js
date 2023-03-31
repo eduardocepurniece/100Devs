ba52783d775c8c816b2426757b8ccbc7189b19bb
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-31 16:50:13 -0400

    Finished objects tony hawk pro skater exercices

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,18 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function TonyHawkCharacterCreator(name, stance, boardType, catchprase){
+    this.name = name;
+    this.stance = stance;
+    this.boardType = boardType;
+    this.catchprase = catchprase;
+
+    this.taunt = function(){
+        console.log('He He He He ' + catchprase);
+    }
+    this.trick = function(){
+        console.log('Performing 360 backslash demon flip');
+    }
+    this.skate = function(){
+        console.log('Performing normal skating');
+    }
+}
