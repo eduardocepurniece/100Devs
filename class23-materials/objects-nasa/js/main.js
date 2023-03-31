f1943be920745b9078dbf1b676b6246a9ce08998
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-31 16:49:13 -0400

    Completed objects nasa

--- class23-materials/objects-nasa/js/main.js
+++ class23-materials/objects-nasa/js/main.js
@@ -1,1 +1,18 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', getPicture);
+
+function getPicture(){
+    const inputDate = document.querySelector('input').value;
+
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=" + inputDate)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('img').src = data.hdurl;
+      document.querySelector('p').innerText = data.explanation;
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
