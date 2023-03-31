e1ab5e9d0dcfd22e32b1e2b0508ddceb342aa15b
educepnieks <84396746+educepnieks@users.noreply.github.com>
2023-03-31 16:48:44 -0400

    Completed objects lost galaxy

--- class23-materials/objects-lost-galaxy/js/main.js
+++ class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a dog object that has four properties and three methods
+let dog = {
+    name : 'terry',
+    breed : 'mixed',
+    favToy : 'football ball',
+    energyLevels : 'high',
+
+    bark : function(){
+        console.log('WOUF! WOUF!');
+    },
+    run : function(){
+        console.log('running like some crazy bastard');
+    },
+    bite : function(){
+        console.log('Have you ever seen a wild beast you were sure it would not bite you? Me, never! -- proceeds to bite -- ')
+    }
+}
