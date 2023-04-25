db846840c22077541d0da42c3981c4bf76fde706
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-04-25 10:00:08 -0400

    Created class MakeDish with 4 properties and 3 methods

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+class MakeDish{
+    constructor(name, ingredients, instructions, prepTime){
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.prepTime = prepTime;
+    }
+    broil(){
+        console.log('fush! fash!');
+    }
+    stir(){
+        console.log('bromgds bramgs brorbso');
+    }
+    fry(){
+        console.log('Tzzzzsssssss...');
+    }
+}
+
+let carpaccio = new MakeDish('Carpaccio', ['meat', 'salt', 'blackPepper'], 'Season the meat with salt and blackPepper and then mince the meat until it forms a thin paste', 15);
