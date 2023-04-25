7dca540f8b22952f807774122802ad13c19eccbe
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-04-25 10:41:50 -0400

    Finished object nasa's api

--- class26-materials/objects-nasa/js/main.js
+++ class26-materials/objects-nasa/js/main.js
@@ -1,1 +1,19 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${choice}`;
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data);
+        document.querySelector('h2').innerHTML = data.title;
+        document.querySelector('img').src = data.hdurl;
+        document.querySelector('h3').innerHTML = data.explanation;
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
