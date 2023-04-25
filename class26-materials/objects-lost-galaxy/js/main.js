7c14d66ee54f72331a951328c90ee062c48307ab
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-04-25 10:18:54 -0400

    Created Doggies class and new dog
    Doggies class is a dog constructor with 4 properties and 3 methods.

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
 //Create a dog object that has four properties and three methods
+class Doggies{
+    constructor(name, breed, size, age){
+        this.name = name;
+        this.breed = breed;
+        this. size = size;
+        this.age = age;
+    }
+    bark(){
+        console.log('Wouf! Wouf!');
+    }
+    poo(){
+        console.log('enggghghhghgh');
+    }
+    markTerritory(){
+        console.log('Psssssss...');
+    }
+}
+
+let apuh = new Doggies('Apuh', 'Schnaicher', 'medium', '14');
