9480206b6b813a63dbed416a69884db57c16a972
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-04-25 10:10:31 -0400

    Added getFetch function
    getFetch function gets data from dog api and displays picture recieved in the dom.

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,16 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+getFetch();
+
+function getFetch(){
+  const url = 'https://dog.ceo/api/breeds/image/random';
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.message;
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
