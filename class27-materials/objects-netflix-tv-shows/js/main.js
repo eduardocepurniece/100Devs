86a510e70e1eec2fa40427992daadf64d8a2fb4d
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-05-04 17:36:56 -0400

    Completed objects-netflix-tv-shows

--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,21 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixShowsMaker{
+    constructor(title, category, rating, episodes){
+        this.title = title;
+        this.category = category;
+        this.rating = rating;
+        this.episodes = episodes;
+    }
+    play(){
+        console.log('playing');
+    }
+    stop(){
+        console.log('stopped');
+    }
+    pause(){
+        console.log('paused');
+    }
+}
 
+let shadowhunters = new NetflixShowsMaker('Shadowhunters', ['Teen', 'Paranormal', 'action'], 4.3, 18);
