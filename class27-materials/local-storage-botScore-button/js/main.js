2ba4ae68b1e60f69c7dbee4e71c6a117a538039f
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-05-04 15:24:38 -0400

    Finished local-storage-botScore-button
    Also added reset button

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,24 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+document.querySelector('#addScore').addEventListener('click',addOne);
+document.querySelector('#resetScore').addEventListener('click',reset);
 
+let scoreCount = 0;
+
+function addOne(){
+    scoreCount += 1;
+    setScore(scoreCount);
+}
+function reset(){
+    scoreCount = 0;
+    setScore(scoreCount);
+}
+function setScore(scoreToSet){
+    localStorage.setItem('score', scoreToSet);
+    document.querySelector('p').innerHTML = scoreToSet;
+}
+
+if(localStorage.getItem('score')){
+    scoreCount = Number(localStorage.getItem('score'));
+}
+document.querySelector('p').innerHTML = scoreCount;
 
