e6479928daa64dc1dbf1b1503b24c868f3ae7c97
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-06-03 16:36:20 -0400

    Completed objects-espresso-machine

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,18 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class ExpressoMaker{
+    constructor(brand, model, price, rating){
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.rating = rating;
+    }
+    turnOn(){
+        console.log('Im turned On');
+    }
+    turnOff(){
+        console.log('Im turned Off');
+    }
+    brew(){
+        console.log('Im brewing');
+    }
+}
