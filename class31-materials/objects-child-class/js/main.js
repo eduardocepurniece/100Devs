c0f4348ea4463d0b410116ab9c81f8e7c40e9550
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-06-04 15:27:25 -0400

    Played with object child class code

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,48 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this._name = name;
+    }
+    get name(){
+        return this._name;
+    }
+    speak(){
+        console.log(`${this.name} makes some kind of noice`);
+    }
+}
+
+class Domesticated extends Animal{
+    constructor(name, breed){
+        super(name);
+        this._breed = breed;
+    }
+}
+
+class Cat extends Domesticated{
+    constructor(name, breed){
+        super(name,breed);
+    }
+    speak(){
+        console.log(`${this.name} meaws`);
+    }
+}
+
+class Dog extends Domesticated{
+    constructor(name, breed){
+        super(name,breed);
+    }
+    speak(){
+        console.log(`${this.name} barks`);
+    }
+}
+
+let simba = new Dog('Simba','Shepard')
+let machi = new Dog('The Machine','Pitbull')
+let salem = new Cat('Salem', 'American Shorthair')
+
+let farm = [simba,machi,salem]
+
+for( a of farm ){
+    a.speak()
+}
