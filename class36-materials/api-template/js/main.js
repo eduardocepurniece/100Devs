fae6c89a5eb715752dc453fc98ec9b8771105d9a
eduardocepurniece <84396746+eduardocepurniece@users.noreply.github.com>
2023-06-09 15:56:53 -0400

    Coded API request using async/await

--- class36-materials/api-template/js/main.js
+++ class36-materials/api-template/js/main.js
@@ -1,1 +1,9 @@
 //Make an api request using async await
+document.querySelector('button').addEventListener('click', getThatDog);
+
+async function getThatDog(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json();
+    console.log(data);
+    document.querySelector('img').src = data.message;
+}
